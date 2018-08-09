1b105ca63af6096b03b4b7e7ecf48fe2fc1c2371
Maksym Harbovskyi <mharbovskyi@luxoft.com>
2018-08-09 11:54:25 +0200

    proper order for injection in android components

--- app/src/main/java/com/mharbovskyi/searchflightstask/view/MainActivity.java
+++ app/src/main/java/com/mharbovskyi/searchflightstask/view/MainActivity.java
@@ -44,14 +44,15 @@ public class MainActivity extends Activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         DaggerActivityComponent
                 .builder()
                 .context(this)
                 .build()
                 .inject(this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
 
         toolbar = findViewById(R.id.my_toolbar);
         toolbar.setTitle(R.string.app_title);
